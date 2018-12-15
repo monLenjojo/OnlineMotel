3c8e2e5f6a0aa39c383abef500735d623ab2d90e
monLenjojo <xo3374ox@gmail.com>
2018-12-15 21:36:39 +0800

    Inti

--- app/src/main/java/com/example/user1801/onlinemotel/LogInActivity.java
+++ app/src/main/java/com/example/user1801/onlinemotel/LogInActivity.java
@@ -13,7 +13,7 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        
+
     }
 
     public void clickLogin(View view){
